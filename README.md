c61ec9b0b0a98981cc4e93ac6813d1f3e94543df
Divya Shet <88928587+Divyashet66@users.noreply.github.com>
2022-12-01 09:27:26 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# note
+
+## to edit/build image and deploy to kubernetes
+sh template-master/template/run.sh
+
+## to create job
+sh template-master/master/modules/run.sh 
---
+++ template/run.sh
@@ -0,0 +1,11 @@
+#to give execute permission
+chmod +x template-master/template/editor/script.sh
+
+#to edit files
+./template-master/template/editor/script.sh
+
+#to give execute permission
+chmod +x template-master/template/mainscript/script.sh
+
+#to build image and deploy to kubernetes
+./template-master//template/mainscript/script.sh
